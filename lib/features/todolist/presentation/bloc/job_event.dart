f9954a24d4958490082960a14f96868878632e3c
devlow03 <nth4356@gmnail.com>
2025-02-24 17:08:27 +0700

    feat: add job event

--- lib/features/todolist/presentation/bloc/job_event.dart
+++ lib/features/todolist/presentation/bloc/job_event.dart
@@ -1,1 +1,25 @@
+import 'package:app_todolist_desktop/features/todolist/domain/entities/job_entity.dart';
 
+sealed class JobEvent {
+  JobEvent();
+}
+
+class AddJobEvent extends JobEvent {
+  final JobEntity job;
+  AddJobEvent({required this.job});
+}
+
+class GetJobsEvent extends JobEvent {
+  GetJobsEvent();
+}
+
+class UpdateJobEvent extends JobEvent {
+  final String id;
+  final JobEntity job;
+  UpdateJobEvent({required this.id, required this.job});
+}
+
+class DeleteJobByIdEvent extends JobEvent {
+  final String id;
+  DeleteJobByIdEvent({required this.id});
+}
