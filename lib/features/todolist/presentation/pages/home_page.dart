0e81d1077ce7f8e940ab0da99b4add5b1c8e0a43
devlow03 <nth4356@gmnail.com>
2025-02-25 10:51:07 +0700

    fix[ui]:change custom scrollview to scaffold

--- lib/features/todolist/presentation/pages/home_page.dart
+++ lib/features/todolist/presentation/pages/home_page.dart
@@ -14,80 +14,73 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            backgroundColor: Colors.white,
-            title: Text("Ứng dụng TodoList"),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all(Colors.blue),
-                    ),
-                    onPressed: () async =>
-                        await showAddJobDialog(context: context),
-                    child: Text(
-                      "Thêm công việc",
-                      style: TextStyle(color: Colors.white),
-                    )),
-              ),
-            ],
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text("Ứng dụng TodoList"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.blue),
+                ),
+                onPressed: () async => await showAddJobDialog(context: context),
+                child: Text(
+                  "Thêm công việc",
+                  style: TextStyle(color: Colors.white),
+                )),
           ),
-          SliverToBoxAdapter(
-            child: BlocConsumer<JobBloc, JobState>(
-              listener: (context, state) {
-                if (kDebugMode) {
-                  print(">>>>>>>>state1: $state");
-                }
-                if (state is JobError) {
-                  if (kDebugMode) {
-                    print(">>>>>>>>error: ${state.message}");
-                  }
-                }
-              },
-              builder: (context, state) {
-                if (kDebugMode) {
-                  print(">>>>>>>>state2: $state");
-                }
-                List<JobEntity> jobs = [];
-                if (state is JobLoading) {
-                  return CircularProgressIndicator();
-                }
-
-                if (state is JobIsEmpty) {
-                  return Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Không có công việc nào",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ],
-                    ),
-                  );
-                }
-                jobs = state is JobSuccess ? state.jobs : [];
-                return GridView.builder(
-                  padding: EdgeInsets.all(8),
-                  shrinkWrap: true,
-                  itemCount: jobs.length,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      childAspectRatio: 10 / 4,
-                      crossAxisSpacing: 10,
-                      // mainAxisExtent: 10,
-                      mainAxisSpacing: 10,
-                      crossAxisCount: 5),
-                  itemBuilder: (context, index) {
-                    return jobWidget(jobs[index], context);
-                  },
-                );
-              },
-            ),
-          )
         ],
+      ),
+      body: BlocConsumer<JobBloc, JobState>(
+        listener: (context, state) {
+          if (kDebugMode) {
+            print(">>>>>>>>state1: $state");
+          }
+          if (state is JobError) {
+            if (kDebugMode) {
+              print(">>>>>>>>error: ${state.message}");
+            }
+          }
+        },
+        builder: (context, state) {
+          if (kDebugMode) {
+            print(">>>>>>>>state2: $state");
+          }
+          List<JobEntity> jobs = [];
+          if (state is JobLoading) {
+            return CircularProgressIndicator();
+          }
+
+          if (state is JobIsEmpty) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Không có công việc nào",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
+            );
+          }
+          jobs = state is JobSuccess ? state.jobs : [];
+          return GridView.builder(
+            padding: EdgeInsets.all(8),
+            shrinkWrap: true,
+            itemCount: jobs.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 10 / 4,
+                crossAxisSpacing: 10,
+                // mainAxisExtent: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 5),
+            itemBuilder: (context, index) {
+              return jobWidget(jobs[index], context);
+            },
+          );
+        },
       ),
     );
   }
